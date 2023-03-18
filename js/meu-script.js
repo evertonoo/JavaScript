a2159e1292b05bdb417f860e3fc5ad78585eb2b3
Everton Luis Vieira <evertonoo@hotmail.com>
2023-03-18 19:24:04 -0300

    implementação do projeto de estudo javascript com o jogo mata mosca no final

---
+++ js/meu-script.js
@@ -0,0 +1,50 @@
+// alert('Ola, seja bem vindo ao curso!!!')
+
+/* selecionar um elemento no DOM 
+atualizar o valor desse elemento com uma string*/ 
+//document.getElementById('nome').value = 'Teste 1 2 3'
+
+//string
+// Tipo texto deve estar dentro de aspas
+var _texto = 'Curso de JavaScript'
+var _numeros =  'sucess'
+// var _nome = prompt( 'Digite o seu nome:') // o comando prompt abrira uma caixa de dialogo e sempre interpretara  o codigo como string
+// var _idade = prompt(' Digite a sua idade:')
+var _idade2 = 50
+//console.log(_idade)
+//console.log(_idade2)
+//null
+var _teste = null
+console.log(_teste)
+
+_teste ='Valor modificado com sucesso'
+console.log(_teste)
+
+_teste =    127.48
+console.log(_teste)
+
+
+//undefined
+var _teste2
+
+//console.log(_teste)
+//console.log(_teste2)
+//number 
+//não se usa dentro de aspas
+//numero com fração sempre separando a fração com "."
+var _numeroInteiro =  -7
+var _numeroFracionado = 10000.75
+
+//boolean
+//São utilizadas em testes condicionais (baseada em algum tipo de condição)
+// podem ser apenas 2 valores TRUE -> valor verdadeiro, False -> valor falso
+
+var _teste = false
+console.log(_teste)
+
+//alert(_teste) //abrir um dialog
+//document.write(_texto)
+//console.log(_numeros)
+//document.write('<h1>Olá ' + _nome + ' tudo bem? Estou vendo aqui que você posssui ' +_idade + ' anos</h1')
+
+
